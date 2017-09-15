2b7f7424652abb2ff1a3cc6456d35d1b6c5d8854
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-15 15:58:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,23 +96,23 @@ function callApi(req, res) {
 var myKeys = Object.keys(flightstatus) 
 newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
                                 //app1.ask(`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd} , Arrival ${estimateda}. Schedule Departure ${scheduledd} , Arrival ${scheduleda}. Do u want to know anything else?`);
-app1.ask(`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd.substring(0, estimatedd.length-4)} , Arrival ${estimateda.substring(0, estimateda.length-4)}. Schedule Departure ${scheduledd.substring(0, scheduledd.length-4)} , Arrival ${scheduleda.substring(0, scheduleda.length-4)}. Do u want to know anything else?`);
+
   app1.askWithList(app1.buildRichResponse()
     .addSimpleResponse(`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd.substring(0, estimatedd.length-4)} , Arrival ${estimateda.substring(0, estimateda.length-4)}. Schedule Departure ${scheduledd.substring(0, scheduledd.length-4)} , Arrival ${scheduleda.substring(0, scheduleda.length-4)}. Do u want to know anything else?`),
     // Build a list
     app1.buildList('list2')
     // Add the first item to the list
-    .addItems(app1.buildOptionItem()
+    .addItems(app1.buildBasicCard()
       .setTitle(`${datedep}`)
       .setDescription(`${scheduledd.substring(0, scheduledd.length-4)}\n\nEst:\n${estimatedd.substring(0, estimatedd.length-4)}`)
        .setImage('https://lh3.googleusercontent.com/K7IBRJz-E1h4gR0wfpcCzwf1MVxV8LXHpqFfKctPdiC54e9GUNTqC_vi_Mhe4KWcB5XnT2ku=w50-h50-e365', 'Departure',50,50)
       )
     // Add the second item to the list
-    .addItems(app1.buildOptionItem()
+    .addItems(app1.buildBasicCard()
       .setTitle(`Arrival`)
       .setDescription(`${scheduleda.substring(0, scheduleda.length-4)}\n\nEst:\n${estimateda.substring(0, estimateda.length-4)}`)
       .setImage('http://grfx.cstv.com/story-nav/icon-plus3.png', 'Recipe')
-    ).addItems(app1.buildOptionItem().setTitle(`${sourcecarrier} => ${destcarrier}`)
+    ).addItems(app1.buildBasicCard().setTitle(`${sourcecarrier} => ${destcarrier}`)
       .setDescription(`${airports} - ${airportd}`)
     )
   );
