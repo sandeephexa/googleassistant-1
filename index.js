49e860ce8eb324ec819ac888bea0263eb0dfb001
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-15 16:25:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -119,33 +119,22 @@ newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
                                 
 //===========================================                                
   app1.askWithList(app1.buildRichResponse()
-    .addSimpleResponse('Alright')
-    .addSuggestions(
-      ['Basic Card', 'List', 'Carousel', 'Suggestions']),
+    .addSimpleResponse('Alright'),
     // Build a list
-    app1.buildList('Things to learn about')
+      app1.buildList('Flight Status')
     // Add the first item to the list
-    .addItems(app1.buildOptionItem('MATH_AND_PRIME',
-      ['math', 'math and prime', 'prime numbers', 'prime'])
-      .setTitle('Math & prime numbers')
-      .setDescription('42 is an abundant number because the sum of its ' +
-        'proper divisors 54 is greater…')
-      .setImage('http://example.com/math_and_prime.jpg', 'Math & prime numbers'))
+    .addItems(app1.buildOptionItem()
+      .setTitle(`${datedep}`)
+      .setDescription(`${scheduledd.substring(0, scheduledd.length-4)}\n\nEst:\n${estimatedd.substring(0, estimatedd.length-4)}`)
+       .setImage('https://lh3.googleusercontent.com/K7IBRJz-E1h4gR0wfpcCzwf1MVxV8LXHpqFfKctPdiC54e9GUNTqC_vi_Mhe4KWcB5XnT2ku=w50-h50-e365', 'Departure',50,50)
+      )
     // Add the second item to the list
-    .addItems(app1.buildOptionItem('EGYPT',
-      ['religion', 'egpyt', 'ancient egyptian'])
-      .setTitle('Ancient Egyptian religion')
-      .setDescription('42 gods who ruled on the fate of the dead in the ' +
-        'afterworld. Throughout the under…')
-      .setImage('http://example.com/egypt', 'Egypt')
-    )
-    // Add third item to the list
-    .addItems(app1.buildOptionItem('RECIPES',
-      ['recipes', 'recipe', '42 recipes'])
-      .setTitle('42 recipes with 42 ingredients')
-      .setDescription('Here\'s a beautifully simple recipe that\'s full ' +
-        'of flavor! All you need is some ginger and…')
-      .setImage('http://example.com/recipe', 'Recipe')
+    .addItems(app1.buildOptionItem()
+      .setTitle(`Arrival`)
+      .setDescription(`${scheduleda.substring(0, scheduleda.length-4)}\n\nEst:\n${estimateda.substring(0, estimateda.length-4)}`)
+      .setImage('http://grfx.cstv.com/story-nav/icon-plus3.png', 'Recipe')
+    ).addItems(app1.buildOptionItem().setTitle(`${sourcecarrier} => ${destcarrier}`)
+      .setDescription(`${airports} - ${airportd}`)
     )
   );
 
