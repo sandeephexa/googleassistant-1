5e74985b918538749a945833d2725c6b27965bfc
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-15 14:44:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
       .setTitle(`Arrival`)
       .setDescription(`${scheduleda.substring(0, scheduleda.length-4)}\n\nEst:\n${estimateda.substring(0, estimateda.length-4)}`)
       .setImage('http://grfx.cstv.com/story-nav/icon-plus3.png', 'Recipe')
-    ).addItems(app1.buildOptionItem('RECIPES1').setTitle(`${sourcecarrier} => ${destcarrier}`)
+    ).addItems(app1.buildList('list4').setTitle(`${sourcecarrier} => ${destcarrier}`)
       .setDescription(`${airports} - ${airportd}`)
     )
   );
