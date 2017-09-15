fff34f57d78774c5d83473397a7752034f2ab82d
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-15 11:43:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -103,16 +103,14 @@ newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
     .addItems(app1.buildOptionItem('MATH_AND_PRIME',
       ['math', 'math and prime', 'prime numbers', 'prime'])
       .setTitle('Dep')
-      .setDescription('42 is an abundant number because the sum of its ' +
-        'proper divisors 54 is greater…')
+      .setDescription(`${scheduledd.substring(0, scheduledd.length-4)}\nEst:${estimatedd.substring(0, estimatedd.length-4)}`)
        .setImage('http://www.clker.com/cliparts/B/X/8/8/p/r/departures-airport-sign-th.png', 'Departure',50,50)
       )
     // Add the second item to the list
     .addItems(app1.buildOptionItem('EGYPT',
       ['religion', 'egpyt', 'ancient egyptian'])
       .setTitle('Arr')
-      .setDescription('42 gods who ruled on the fate of the dead in the ' +
-        'afterworld. Throughout the under…')
+      .setDescription(`${scheduledd.substring(0, scheduledd.length-4)}\nEst:${estimatedd.substring(0, estimatedd.length-4)}`)
       .setImage('http://www.drivesafe.org.nz/themes/drivesafeorgnz-theme/images/arrival-icon.svg', 'Egypt',25,25)
     )
     // Add third item to the list
