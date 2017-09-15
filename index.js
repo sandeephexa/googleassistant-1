33eab097b0e2d3c4da8c0c0ed0ceeb43fc14792d
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-15 14:33:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ function callApi(req, res) {
                                 let citys = source.city;
                                 let countrys = source.countryName;
                                 let airportd = destination.name;
+                                let sourcecarrier=fligarriv.flightStatuses[0].departureAirportFsCode;
+                                let destcarrier=fligarriv.flightStatuses[0].arrivalAirportFsCode;
                                 let cityd = destination.city;
                                 let countryd = destination.countryName;
                                 let flightstatuses = fligarriv.flightStatuses[0].status;
@@ -112,7 +114,7 @@ newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
       .setTitle(`Arrival`)
       .setDescription(`${scheduleda.substring(0, scheduleda.length-4)}\n\nEst:\n${estimateda.substring(0, estimateda.length-4)}`)
       .setImage('http://grfx.cstv.com/story-nav/icon-plus3.png', 'Recipe')
-    ).addItems(app1.buildOptionItem('RECIPES1').setTitle(`${countrys} => \n ${countryd}`)
+    ).addItems(app1.buildOptionItem('RECIPES1').setTitle(`${sourcecarrier} => ${destcarrier}`)
       .setDescription(`${airports} - ${airportd}`)
     )
   );
