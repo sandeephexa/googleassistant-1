b21185c0667362b0f893a658a54f250d3f8230f5
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-15 11:03:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,21 +93,21 @@ var myKeys = Object.keys(flightstatus)
 newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
                                 //app1.ask(`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd} , Arrival ${estimateda}. Schedule Departure ${scheduledd} , Arrival ${scheduleda}. Do u want to know anything else?`);
 //app1.ask(`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd.substring(0, estimatedd.length-4)} , Arrival ${estimateda.substring(0, estimateda.length-4)}. Schedule Departure ${scheduledd.substring(0, scheduledd.length-4)} , Arrival ${scheduleda.substring(0, scheduleda.length-4)}. Do u want to know anything else?`);
-app.askWithList(app.buildRichResponse()
+app1.askWithList(app1.buildRichResponse()
     .addSimpleResponse('Alright')
     .addSuggestions(
       ['Basic Card', 'List', 'Carousel', 'Suggestions']),
     // Build a list
-    app.buildList('Things to learn about')
+    app1.buildList('Things to learn about')
     // Add the first item to the list
-    .addItems(app.buildOptionItem('MATH_AND_PRIME',
+    .addItems(app1.buildOptionItem('MATH_AND_PRIME',
       ['math', 'math and prime', 'prime numbers', 'prime'])
       .setTitle('Math & prime numbers')
       .setDescription('42 is an abundant number because the sum of its ' +
         'proper divisors 54 is greater…')
       .setImage('http://example.com/math_and_prime.jpg', 'Math & prime numbers'))
     // Add the second item to the list
-    .addItems(app.buildOptionItem('EGYPT',
+    .addItems(app1.buildOptionItem('EGYPT',
       ['religion', 'egpyt', 'ancient egyptian'])
       .setTitle('Ancient Egyptian religion')
       .setDescription('42 gods who ruled on the fate of the dead in the ' +
@@ -115,7 +115,7 @@ app.askWithList(app.buildRichResponse()
       .setImage('http://example.com/egypt', 'Egypt')
     )
     // Add third item to the list
-    .addItems(app.buildOptionItem('RECIPES',
+    .addItems(app1.buildOptionItem('RECIPES',
       ['recipes', 'recipe', '42 recipes'])
       .setTitle('42 recipes with 42 ingredients')
       .setDescription('Here\'s a beautifully simple recipe that\'s full ' +
